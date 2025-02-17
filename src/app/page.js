1b9b1ede48404c317c88740a5e305d829f55563a
Emotinnn <phanuwatpanyajob@gmail.com>
2025-02-17 22:20:22 +0700

    fix texr

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 // Components
 import NavBar from "./components/NavBar";
-import Banner from "./components/ฺBanner";
+import Banner from "./components/Banner";
 
 export default function Home() {
   return (
