6cc0796d4b5da27f097e45487b92c1438ca9985a
Emotinnn <phanuwatpanyajob@gmail.com>
2025-04-01 17:24:34 +0700

    fix font

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -5,10 +5,10 @@ const NavBar = () => {
         <nav className="bg-black p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="flex space-x-4">
-                    <a href="#" className="text-white hover:text-blue-400">Home</a>
-                    <a href="#" className="text-white hover:text-blue-400">About</a>
-                    <a href="#" className="text-white hover:text-blue-400">Services</a>
-                    <a href="#" className="text-white hover:text-blue-400">Contact</a>
+                    <a href="#" className="font-merriweather text-white hover:text-blue-400">Home</a>
+                    <a href="#" className="font-merriweather text-white hover:text-blue-400">About</a>
+                    <a href="#" className="font-merriweather text-white hover:text-blue-400">Services</a>
+                    <a href="#" className="font-merriweather text-white hover:text-blue-400">Contact</a>
                 </div>
                 <div className="text-white"> 
                     <a href="tel:+66931345179" className="hover:text-blue-400 flex" >
